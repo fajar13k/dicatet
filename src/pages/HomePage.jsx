929aac3071400461f49cd697ef2ab645aa0008fb
fajar13k <fajar.fj13@gmail.com>
2022-09-18 22:41:10 +0700

    Conditional rendering on active/archived notes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,9 +9,9 @@ function HomePage() {
   return (
     <section>
       <h2 className="font-semibold text-xl mb-4">Active Notes</h2>
-      <NoteList notes={activeNotes} />
+      {activeNotes.length > 0 ? (<NoteList notes={activeNotes} />) : (<p>No active notes!</p>)}
       <h2 className="font-semibold text-xl my-4">Archived Notes</h2>
-      <NoteList notes={archivedNotes} />
+      {archivedNotes.length > 0 ? (<NoteList notes={archivedNotes} />) : (<p>No archived notes!</p>)}
     </section>
   );
 }
