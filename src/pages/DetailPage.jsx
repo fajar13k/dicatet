94277b0cdaad09960d11f5415b3f890dc9f13246
fajar13k <fajar.fj13@gmail.com>
2022-09-22 17:32:25 +0700

    Handle detail page not loading

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { getNote } from '../utils/local-data';
-import NoteDetail from '../components/NoteDetail';
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { getNote } from "../utils/local-data";
+import NoteDetail from "../components/NoteDetail";
+import PageNotFound from "./pages/_404";
+import { useParams } from "react-router-dom";
 
 export default function DetailPage() {
   const { id } = useParams();
   const [note, setNotes] = useState(getNote(id));
 
   return (
-    <NoteDetail note={note} />
-  )
+    <>{note === undefined ? <PageNotFound /> : <NoteDetail note={note} />}</>
+  );
 }
