17be67897709ee49fe4abaa61c314dd28571d78c
Fajar Nazmi Fadillah <fajar.fj13@gmail.com>
2022-10-26 22:11:27 +0700

    Fix 404 page not linking

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { getNote } from "../utils/local-data";
 import NoteDetail from "../components/NoteDetail";
-import PageNotFound from "./pages/_404";
+import PageNotFound from "../pages/_404";
 import { useParams } from "react-router-dom";
 
 export default function DetailPage() {
