aecc2a87f3c582c48151349902b4fef2f656d1ed
Fajar Nazmi Fadillah <fajar.fj13@gmail.com>
2022-10-28 12:19:04 +0700

    Add locale dates

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,25 @@
 const showFormattedDate = (date) => {
+  let localeFormat;
+  const locale = localStorage.getItem('locale');
+  
+  switch (locale) {
+    case 'en':
+      localeFormat = "fr-FR";
+      break;
+    case 'id':
+      localeFormat = "id-ID";
+      break;
+    default:
+      localeFormat = "en-GB";
+  }
+
   const options = {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   };
-  return new Date(date).toLocaleDateString('id-ID', options);
+  return new Date(date).toLocaleDateString(localeFormat, options);
 };
 
 export { showFormattedDate };
