e2c386e53a3ee421a348bc7a11e060a2c8f59e0c
Fajar Nazmi Fadillah <fajar.fj13@gmail.com>
2022-10-28 12:06:05 +0700

    Add french locale support

--- src/languages/index.js
+++ src/languages/index.js
@@ -1,9 +1,11 @@
 import en from './en.json';
+import fr from './fr.json';
 import id from './id.json';
 
-export const dictionaryList = { en, id };
+export const dictionaryList = { en, fr, id };
 
 export const languageOptions = {
   en: 'English',
+  fr: 'français',
   id: 'Bahasa Indonesia'
 };
